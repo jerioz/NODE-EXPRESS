feacab48d9ad320d98a92004b086e3ce247e11fb
jerioz <escobedoranero@gmail.com>
2023-04-27 11:41:45 +0200

    ej-2 ok

--- ej-2/ej2.js
+++ ej-2/ej2.js
@@ -2,7 +2,7 @@ const express= require('express');
 const app = express();
 
 
-app.listen("3000", (req, res) => {
+app.listen("3000", () => {
     console.log('Servidor abierto en el puerto 3000')
 })
 
